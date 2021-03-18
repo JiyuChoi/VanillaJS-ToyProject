917f6c28d300fed14ae7f15f7bac0c7bfa0a1b70
choijiyu <jiyou126@naver.com>
2021-03-18 19:38:45 +0900

    add get weather and location

--- ChormeApp/weather.js
+++ ChormeApp/weather.js
@@ -1,4 +1,18 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "39c453653738e03164dd449123eb9a3e";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function (response) {
+        return response.json()
+    }).then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +26,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,6 +43,8 @@ function loadCoords() {
         askForCoords();
     } else {
         // getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
