878c78f0146b6215557978022332dbe09a35109e
choijiyu <jiyou126@naver.com>
2021-03-23 14:04:46 +0900

    add function color and size of brush

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -1,16 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 // Give canvas width, height (pixel)
 canvas.width = 700;
 canvas.height = 700;
 
+// default 설정
 // Color of line
 ctx.strokeStyle = "#2c2c2c";
 // Pixel of line
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainiting() {
     painting = false;
@@ -34,8 +39,26 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    // override
+    ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    // override
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filiing = true;
+        mode.innerText = "Paint";
+    }
 }
 
 if (canvas) {
@@ -43,4 +66,15 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainiting);
     canvas.addEventListener("mouseleave", stopPainiting);
+}
+
+// Array.from: object -> array
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if (range) {
+    range.addEventListener("input", handleRangeChange)
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
 }
